bc743b785138d38442d986c3a94511cbe1c6e838
unknown <keane.candra16@gmail.com>
2025-11-15 16:33:32 +0700

    nambah method selectParentByRank() sama benerin doCrossover()

--- Genetic Algorithm/Population.java
+++ Genetic Algorithm/Population.java
@@ -5,7 +5,7 @@ public class Population {
     private int maxPopulationSize;
     private int populationSize = 0;
     public double elitismPct;
-    //ArrayList<Item> listOfItems;
+    // ArrayList<Item> listOfItems;
     int maxCapacity;
     int sumRank = 0;
     Random MyRand;
@@ -52,7 +52,7 @@ public class Population {
         // Panggil konstarktor population baru
         Population newPop = new Population(this.MyRand, this.maxCapacity, this.populationSize, this.elitismPct);
         int n = (int) (this.elitismPct * this.maxPopulationSize);
-        
+
         // System.out.println(n);
         // System.out.println(this.population.size());
 
@@ -93,18 +93,19 @@ public class Population {
      */
 
     // pemilihan 2 parent dengan teknik roulette wheel
-    public Individual[] selectParent() { 
+    public Individual[] selectParent() {
         Individual[] parents = new Individual[2];
         // this.population.sort((idv1,idv2) -> idv1.compareTo(idv2));
 
-        //int top = this.population.size() + 1;
+        // int top = this.population.size() + 1;
         long sumfitness = 0;
         for (int i = 0; i < this.population.size(); i++) {
             sumfitness = sumfitness + this.population.get(i).fitness;
         }
         // System.out.println(sumfitness);
         for (int i = 0; i < this.population.size(); i++) {
-            ((Individual) this.population.get(i)).parentProbability = 1 -((1.0 * this.population.get(i).fitness) / sumfitness);
+            ((Individual) this.population.get(i)).parentProbability = 1
+                    - ((1.0 * this.population.get(i).fitness) / sumfitness);
         }
 
         // Kode untuk roulette
@@ -118,6 +119,50 @@ public class Population {
             } while (sum < prob);
             parents[n] = this.population.get(i);
         }
+        return parents;
+    }
+
+    public Individual[] selectParentByRank() {
+        Individual[] parents = new Individual[2];
+        int N = this.population.size();
+
+        // sort populasi berdasarkan fitnessnya
+        Collections.sort(this.population);
+
+        // tetapkan probabilitas berdasarkan Peringkat (Rank).
+        // individu terbaik (index 0) mendapat peringkat N.
+        // individu terburuk (index N-1) mendapat peringkat 1.
+        if (this.sumRank == 0) { // Pengaman jika sumRank tidak terinisialisasi
+            for (int i = 1; i <= N; i++)
+                this.sumRank += i;
+        }
+
+        for (int i = 0; i < N; i++) {
+            // i = 0 adalah yang terbaik, i = N-1 adalah yang terburuk.
+            int rank = N - i; // Peringkat terbalik (terbaik = N, terburuk = 1)
+            this.population.get(i).parentProbability = (double) rank / this.sumRank;
+        }
+
+        // 3. lakukan "Roulette Wheel" pada Peringkat (Rank).
+        // loop ini memilih 2 parent.
+        for (int n = 0; n < 2; n++) {
+            double prob = this.MyRand.nextDouble();
+            double cumulativeSum = 0.0;
+            int i = 0; // mulai dari individu terbaik
+
+            // loop untuk menemukan parent berdasarkan prob
+            while (i < N - 1) { // berhenti di N-1 untuk menghindari out of bounds
+                cumulativeSum += this.population.get(i).parentProbability;
+                if (cumulativeSum >= prob) {
+                    break;
+                }
+                i++;
+            }
+            // Jika loop selesai (karena prob sangat tinggi atau error floating point),
+            // kita pilih individu terakhir (i akan bernilai N-1).
+            parents[n] = this.population.get(i);
+        }
+
         return parents;
     }
 
